d73080f244f57c70989a62b06bfd6aabd0781c24
IconLivin <anton.parole@mail.ru>
2021-05-17 02:26:05 +0300

    Added power regression with some samples

--- regression.py
+++ regression.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from numpy import matrix, array, dot, insert
+from numpy import matrix, array, insert, sum, power, log, linspace, identity, zeros_like
 from numpy.linalg import pinv
 
 
@@ -12,12 +12,12 @@ class Model(ABC):
     def __init__(self, X, Y):
         if len(matrix(X)) != len(Y):
             raise ModelException
-        self.X = insert(matrix(X).T, 0, 1, axis=0).T
-        self.Y = array(Y)
+        self.X = insert(matrix(X, dtype=float).T, 0, 1, axis=0).T
+        self.Y = array(Y, dtype=float)
         self.b = []
 
     @abstractmethod
-    def calculate_b(self):
+    def fit(self, respConv=False):
         pass
 
     def __getattribute__(self, name):
@@ -28,6 +28,78 @@ class LinearRegression(Model):
     def __init__(self, X, Y):
         super().__init__(X, Y)
 
-    def calculate_b(self):
+    def fit(self, respConv=False):
+        if respConv:
+            self.Y, self.teta = responseConversion(self.X, self.Y)
         self.b = pinv(self.X).dot(matrix(self.Y).T).T.tolist()[0]
+        self.yHat = (self.X * matrix(self.b).T).T.tolist()[0]
+        self.yAv = sum(self.Y) / len(self.Y)
+        self.R2 = 1 - (sum(power(self.Y - self.yHat, 2))) / (sum(power(self.Y - self.yAv, 2)))
+        self.S2 = sum(power(self.Y - self.yHat, 2)) / (len(self.Y) - len(self.X.T) - 1)
+        self.F = (
+            (matrix(self.b) * self.X.T * matrix(self.Y).T - (sum(self.Y) ** 2) / len(self.Y))
+            / (len(self.X.T) - 1)
+            / self.S2
+        ).tolist()[0][0]
         return self.b
+
+
+class PowerRegression(Model):
+    def __init__(self, X, Y):
+        super().__init__(X, Y)
+
+    def fit(self, pow=2, respConv=False):
+        if respConv:
+            self.Y, self.teta = responseConversion(self.X, self.Y)
+        self.b = (
+            pinv(power(self.X, pow, out=zeros_like(self.X), where=(self.X != 0)))
+            .dot(matrix(self.Y).T)
+            .T.tolist()[0]
+        )
+        self.yHat = (
+            power(self.X, pow, out=zeros_like(self.X), where=(self.X != 0)) * matrix(self.b).T
+        ).T.tolist()[0]
+        self.yAv = sum(self.Y) / len(self.Y)
+        self.R2 = 1 - (sum(power(self.Y - self.yHat, 2))) / (sum(power(self.Y - self.yAv, 2)))
+        self.S2 = sum(power(self.Y - self.yHat, 2)) / (len(self.Y) - len(self.X.T) - 1)
+        self.F = (
+            (
+                matrix(self.b)
+                * power(self.X.T, pow, out=zeros_like(self.X.T), where=(self.X.T != 0))
+                * matrix(self.Y).T
+                - (sum(self.Y) ** 2) / len(self.Y)
+            )
+            / (len(self.X.T) - 1)
+            / self.S2
+        ).tolist()[0][0]
+        return self.b
+
+
+def responseConversion(X, Y):
+    if abs(max(Y) / min(Y)) > 10:
+        lambdas = linspace(-2, 2, num=1000)
+        if min(Y) < 0:
+            print("Negative value is found on Y, use Y without conversion")
+            return Y, 1
+        W = [log(Y) if la == 0 else (power(Y, la) - 1) / la for la in lambdas]
+        middle = identity(len(Y)) - X * pinv(X)
+        lambda_max = [
+            (
+                -0.5 * len(Y) * log(matrix(w) * middle * matrix(w).T / len(Y))
+                + (la - 1) * sum(log(Y))
+            ).tolist()[0][0]
+            for w, la in zip(W, lambdas)
+        ]
+        teta = (
+            round(lambdas[lambda_max.index(max(lambda_max))] * 2.0, 1) / 2.0
+            if abs(lambdas[lambda_max.index(max(lambda_max))]) >= 5e-1
+            else 0
+        )
+        Y = log(Y) if teta == 0 else (power(Y, teta) - 1) / teta
+        return Y, teta
+    else:
+        print(
+            f"The max to min ratio is too small to use response conversion: \
+            {max(Y)} / {min(Y)} = {max(Y) / min(Y)}"
+        )
+        return Y, 1
--- test.py
+++ test.py
@@ -1,7 +1,28 @@
-from regression import LinearRegression
-from numpy import matrix
+from regression import LinearRegression, PowerRegression
+from random import randint
+from matplotlib import pyplot as plt
+import numpy as np
+
+X = []
+Y = []
+
+"""with open(r"C:\Course\regression\Data.txt", "r") as f:
+    for line in f.read().split("\n"):
+        val = line.split(" ")
+        X.append(val[:-1])
+        Y.append(val[-1])"""
+
+for p1 in np.linspace(1, 10):
+    Y.append(14 + 11 / (p1 ** 2) + randint(-1, 1))
+    X.append([p1])
 
 
-l = LinearRegression([[0, 1, 2], [1, 1, 1]], [5, 2])
+l = PowerRegression(X, Y)
+b = l.fit(respConv=True, pow=-2)
 
-print(l.calculate_b())
+fig, ax = plt.subplots(figsize=(8, 6))
+
+
+ax.scatter(X, Y, label="Experiments")
+ax.plot(X, l.yHat)
+plt.show()
